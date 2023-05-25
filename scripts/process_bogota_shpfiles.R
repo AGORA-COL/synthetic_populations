2762ff914a0e12fddb2e200fa596d7de97f1b3d1
Johan M. Calderón <jmcalderonr83@gmail.com>
2023-05-25 08:47:23 -0500

    Start to change the commands from gdal package
    Commands in gdal and sp packages have problems and it would be necessary to migrate to a new package as "sf"

--- scripts/process_bogota_shpfiles.R
+++ scripts/process_bogota_shpfiles.R
@@ -10,7 +10,7 @@ library(tidyverse)
 library(rjson)
 library(sf)
 library(raster)
-library(rgdal)
+library(rgdal) #Please note that rgdal will be retired during 2023
 library(maptools)
 library(osmdata)
 library(RColorBrewer)
@@ -23,22 +23,31 @@ options(digits = 20,scipen = 999)
 ##===============================================================#
 ## Shapefiles-------------
 ##===============================================================#
-upz_shp = rgdal::readOGR('../data/raw_data/geodata/UPZ_Bogota/UPla.shp')
-localities_shp = rgdal::readOGR('../data/raw_data/geodata/localidades_bogota/poligonos-localidades.shp')
-esc_shp = rgdal::readOGR('../data/raw_data/geodata/scat_shp/scat_shp.shp')
-esc_data = esc_shp@data[,c('SCACODIGO', 'SCANOMBRE')]
+# upz_shp = rgdal::readOGR('data/raw_data/geodata/UPZ_Bogota/UPla.shp')
+upz_shp = sf::st_read('data/raw_data/geodata/UPZ_Bogota/UPla.shp')
+# localities_shp = rgdal::readOGR('data/raw_data/geodata/localidades_bogota/poligonos-localidades.shp')
+localities_shp = sf::st_read('data/raw_data/geodata/localidades_bogota/poligonos-localidades.shp')
+# esc_shp = rgdal::readOGR('data/raw_data/geodata/scat_shp/scat_shp.shp')
+esc_shp = sf::st_read('data/raw_data/geodata/scat_shp/scat_shp.shp')
+# esc_data = esc_shp@data[,c('SCACODIGO', 'SCANOMBRE')]
+esc_data = esc_shp[,c('SCACODIGO', 'SCANOMBRE')]
 esc_data$Localidad = 0
 
-esc_coor = coordinates(esc_shp)
+#esc_coor = coordinates(esc_shp)
+esc_coor = sf::st_coordinates(esc_shp)[,1:2]
 colnames(esc_coor) = c("LAT", "LON")
 esc_coor = as.data.frame(esc_coor)
-coordinates(esc_coor) = ~ LAT + LON
-proj4string(esc_coor) = proj4string(localities_shp)
+#coordinates(esc_coor) = ~ LAT + LON
+esc_coor <- st_as_sf(esc_coor, coords = c("LAT", "LON"))
+#proj4string(esc_coor) = proj4string(localities_shp)
+st_crs(esc_coor) <- st_crs(localities_shp)
 
+#Continue the migration to sf
 for(ll in 1:nrow(localities_shp)){
     print(ll)
-    esc_locality = sp::over(esc_coor, localities_shp[ll,])
-    esc_data$Localidad[which(!is.na(esc_locality[,1]))] = localities_shp@data$Identificad[ll]
+    #esc_locality = sp::over(esc_coor, localities_shp[ll,])
+    esc_locality = sf::st_join(esc_coor, localities_shp[ll,])
+    esc_data$Localidad[which(!is.na(esc_locality[,1]))] = localities_shp$Identificad[ll]
 }
 
 esc_data$Localidad[esc_data$SCACODIGO == '108108'] = 1
@@ -46,9 +55,9 @@ esc_shp@data$Localidad = esc_data$Localidad
 esc_list = unique(as.character(esc_data$SCACODIGO))
 esc_shp@data$ZONE = as.character(esc_shp@data$SCACODIGO)
 
-write_csv(esc_data, '../data/processed_data/geodata/Localidad_Unidad_Catastral.csv')
+write_csv(esc_data, 'data/processed_data/geodata/Localidad_Unidad_Catastral.csv')
 
-block_shp = rgdal::readOGR('../data/processed_data/geodata/manzanas_bogota/manzanas_bogota.shp')
+block_shp = rgdal::readOGR('data/processed_data/geodata/manzanas_bogota/manzanas_bogota.shp')
 block_coor = coordinates(block_shp)
 colnames(block_coor) = c("LAT", "LON")
 block_coor = as.data.frame(block_coor)
@@ -64,14 +73,14 @@ for(ee in 1:nrow(esc_shp)){
 
 block_shp@data$SCACODIGO = block_data$SCACODIGO
 
-write_csv(block_data, '../data/processed_data/geodata/Manzana_Unidad_Catastral.csv')
+write_csv(block_data, 'data/processed_data/geodata/Manzana_Unidad_Catastral.csv')
 
 
 ##===============================================================#
 ## UPZ Shapefiles-------------
 ##===============================================================#
-upz_shp = rgdal::readOGR('../data/raw_data/geodata/UPZ_Bogota/UPla.shp')
-esc_shp = rgdal::readOGR('../data/raw_data/geodata/scat_shp/scat_shp.shp')
+upz_shp = rgdal::readOGR('data/raw_data/geodata/UPZ_Bogota/UPla.shp')
+esc_shp = rgdal::readOGR('data/raw_data/geodata/scat_shp/scat_shp.shp')
 esc_data = esc_shp@data[,c('SCACODIGO', 'SCANOMBRE')]
 esc_data$upz = ""
 esc_coor = coordinates(esc_shp)
@@ -91,5 +100,5 @@ if(length(which(esc_data$upz == "")) > 0){
 }
 
 
-write_csv(esc_data, '../data/processed_data/geodata/UPZ_Unidad_Catastral.csv')
+write_csv(esc_data, 'data/processed_data/geodata/UPZ_Unidad_Catastral.csv')
 
